ff21b92bff338c19fdeb53045905ca82099df02d
rohitgeddam <rohitgeddam2018@gmail.com>
2020-12-25 16:37:56 +0530

    make header responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,35 +2,28 @@ import '../styles/Header.scss'
 function Header() {
     return (
   
-        <nav class="navbar is-transparent is-spaced is-dark">
-<div class="navbar-brand">
-<a class="navbar-item" href="https://bulma.io">
-    <div class="navbar-heading">Bitter</div>
-{/* <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" /> */}
-</a>
-<div class="navbar-burger" data-target="navbarExampleTransparentExample">
-<span></span>
-<span></span>
-<span></span>
-</div>
-</div>
-
-<div id="navbarExampleTransparentExample" class="navbar-menu">
-<div class="navbar-start">
-<a class="navbar-item" href="https://bulma.io/">
-  Projects
-</a>
-<a class="navbar-item" href="https://bulma.io/">
-  Confessions
-</a> 
-{/* <a class="navbar-item" href="https://bulma.io/">
-  Home
-</a> */}
-</div>
+    <nav class="navbar is-transparent is-spaced is-dark is-mobile">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+            <div class="navbar-heading">Bitter</div>
+        {/* <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" /> */}
+        </a>
+      </div>
 
 
-</div>
-</nav>
+        <div class="navbar-start">
+          <a class="navbar-item" href="#">
+            <p class="nav-item__link">Projects</p>
+          </a>
+          {/* <a class="navbar-item" href="https://bulma.io/">
+            Confessions
+          </a>  */}
+        </div>
+
+
+
+
+    </nav>
   
     );
   }

69c7b503d2c521ec7e72274acf4e4a717623f0ef
rohitgeddam <rohitgeddam2018@gmail.com>
2020-12-26 12:42:50 +0530

    style error box and fetch user on reload

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import { useQuery, gql } from '@apollo/client' 
 import UserContext from './UserContext'
 
 import Error from "./pages/Error"
@@ -9,9 +9,52 @@ import Header from "./components/Header";
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ProjectsPage from './pages/ProjectsPage'
+import { AUTH_TOKEN } from './constants'
+
+const GET_USER_QUERY = gql`
+  query getUser {
+    me {
+      status
+      message
+      data {
+        id
+        username
+        email
+      }
+    }
+}
+
+`
+
 function App() {
 
   const [user, setUser] = useState()
+  const { loading, error, data } = useQuery(GET_USER_QUERY, {
+    onCompleted: ({me}) => {
+
+      if (me.status === 'Success') {
+        const user = me.data
+        setUser({
+          email: user.email,
+          username: user.username,
+          id: user.id,
+        })
+      }
+    }
+  });
+
+  if (loading)  {
+
+    return <div class="loading">
+      Loading...
+    </div>
+  }
+
+  if (error) {
+    // delete the invalid token
+    localStorage.removeItem(AUTH_TOKEN)
+  }
+
 
   return (
 
@@ -24,12 +67,6 @@ function App() {
               <Route path="/auth/signin" component={LoginPage}/>
               <Route path="/auth/signup" component={RegisterPage}/>
               <Route component={Error} />
-            {/* <Route
-              path='/dashboard'
-              render={(props) => (
-                <Dashboard {...props} isAuthed={true} />
-              )}
-            /> */}
           </Switch>
           </UserContext.Provider>
 
